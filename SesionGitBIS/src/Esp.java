5943705f88f003824bdeae4c2db0d72f49994962
CGP <cgallardo@etsisi.upm.es>
2017-03-14 11:49:35 +0100

    Add Esp class of Spanish greetings

---
+++ SesionGitBIS/src/Esp.java
@@ -0,0 +1,13 @@
+
+public class Esp {
+	String nombre;
+	
+	public Esp(){
+		nombre = "Pepe";
+	};
+	
+	public void saludar (){
+		System.out.println("Hola " + this.nombre);
+	};
+
+}
--- SesionGitBIS/src/Saludos.java
+++ SesionGitBIS/src/Saludos.java
@@ -1,8 +1,10 @@
 
-
 public class Saludos {
 	public static void main(String[] args) {
 		System.out.println("Hola.");
 		System.out.println("Adios.");
+		
+		Esp saludos = new Esp();
+		saludos.saludar();
 	}
 }
