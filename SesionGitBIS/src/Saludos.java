97fad79c1a2981ac0cbbf612b299b982ab7bb9aa
FicticioMES <carolina.gallardo.perez@gmail.com>
2017-03-14 15:56:57 +0100

    Update Saludos.java

--- SesionGitBIS/src/Saludos.java
+++ SesionGitBIS/src/Saludos.java
@@ -1,7 +1,7 @@
 
 public class Saludos {
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos al sistema de saludos.");
+		System.out.println("Willkomen.");
 		Esp saludos = new Esp();
 		saludos.saludar();
 	}
