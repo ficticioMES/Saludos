07824fd1d069f331adbd79f835880f0cd562b8a6
CGP <cgallardo@etsisi.upm.es>
2017-03-14 16:05:41 +0100

    Prompt english

--- SesionGitBIS/src/Saludos.java
+++ SesionGitBIS/src/Saludos.java
@@ -1,7 +1,7 @@
 
 public class Saludos {
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos al sistema de saludos.");
+		System.out.println("Wellcome.");
 		Esp saludos = new Esp();
 		saludos.saludar();
 	}
