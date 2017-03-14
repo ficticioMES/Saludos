9497f5bbfcfcef2fdf6094dab645dab694b9d798
FicticioMES <carolina.gallardo.perez@gmail.com>
2017-03-14 11:57:30 +0100

    Change in main message

--- SesionGitBIS/src/Saludos.java
+++ SesionGitBIS/src/Saludos.java
@@ -1,8 +1,8 @@
 
 public class Saludos {
 	public static void main(String[] args) {
-		System.out.println("Hola.");
-		System.out.println("Adios.");
+		System.out.println("Bienvenidos al sistema de saludos.");
+	
 		
 		Esp saludos = new Esp();
 		saludos.saludar();
